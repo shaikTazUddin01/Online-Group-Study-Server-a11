1749460521bf0c8e0dd7df8233901453b20db8a6
sheikhtazuddin <tazahmedsoft@gmail.com>
2023-11-05 18:46:01 -0800

    assignment is inserted into the database

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
-const port = process.env.PORT || 5000
 require('dotenv').config()
-
+const port = process.env.PORT || 5000
 //middleware
 app.use(express.json())
-app.use(cors());
-
+app.use(cors())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.25fgudl.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -21,37 +18,35 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
-
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
+        // create connection dataBase and collection;
+        const onlineStudyCollection = client.db('onlineStudy').collection('assignment')
+
         app.post('/createAssignment', async (req, res) => {
-            const newAssignment = req.body
-
-            console.log(newAssignment)
-            res.send(newAssignment)
+            const newAssignment = req.body;
+            const result = await onlineStudyCollection.insertOne(newAssignment)
+            console.log(result)
+            res.send(result)
         })
+      
 
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
-
-
-
-
-
 app.get('/', (req, res) => {
-    res.send("Online Group study server is connected")
+    res.send('server is connected')
 })
+
 
 app.listen(port, () => {
-    console.log('Running Port is:', port)
+    console.log("the running port is", port)
 })
-
